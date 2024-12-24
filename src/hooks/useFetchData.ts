407f78487acc2b1bf001ba250937d12f1671340e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2024-12-24 10:25:36 -0800

    add use

---
+++ src/hooks/useFetchData.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { fetchAllData, DataItem } from '../services/dataService';
+
+const useFetchData = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await fetchAllData();
+        setData(result);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, isLoading, error };
+};
+
+export default useFetchData;
